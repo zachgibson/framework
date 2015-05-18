f2cbdbd37c3dc3a7f41774ac686a5212fb074b8b
Zachary Gibson <zacharygibson@Zacharys-iMac.local>
2015-05-18 13:11:35 -0500

    updated gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,35 +8,49 @@ module.exports = function(grunt) {
       app: {
         files: [{
           expand: true,
-          cwd: 'scss',
+          cwd: 'sass',
           src: ['*.scss'],
-          dest: 'css',
+          dest: '/Volumes/WWWShare/html/site/site/wp-content/themes/nwseo-site',
           ext: '.css'
         }]
       },
       options: {
+        sourceComments: true,
         sourceMap: true,
         outputStyle: 'nested',
         imagePath: "../",
       }
     },
 
-    watch: {
-      sass: {
-        files: ['scss/{,*/}*.{scss,sass}'],
-        tasks: ['sass']
-      },
-      options: {
-        livereload: true,
-        spawn: false
+    // Parse CSS and add vendor-prefixed CSS properties using the Can I Use database
+    autoprefixer: {
+      no_dest: {
+        src: '/Volumes/WWWShare/html/site/site/wp-content/themes/nwseo-site/style.css'
       }
     },
+
+    // Watch files for changes and reload the browser once a file changes
+    watch: {
+      options: { livereload: true, },
+      sass: {
+        files: ['sass/{,*/}*.{scss,sass}'],
+        tasks: ['sass'],
+      },
+      css: {
+        files: ['/Volumes/WWWShare/html/site/site/wp-content/themes/nwseo-site/style.css'],
+        tasks: ['autoprefixer']
+      },
+      php: {
+        files: ['/Volumes/WWWShare/html/site/site/wp-content/themes/nwseo-site/*.php'],
+      }
+    }
   });
+
+  // Default task(s).
+  grunt.registerTask('default', ['sass', 'watch']);
 
   // Loads Grunt Tasks
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-
-  // Default task(s).
-  grunt.registerTask('default', ['sass', 'watch']);
+  grunt.loadNpmTasks('grunt-autoprefixer');
 };
